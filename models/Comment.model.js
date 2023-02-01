c64af4fd4c2884e97df3498361ba7fa21983662e
Iryna1791 <irynawebdev@gmail.com>
2023-02-01 01:06:28 +0100

    stylying

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -2,7 +2,8 @@ const { Schema, model, SchemaTypes } = require('mongoose');
 
 const commentSchema = new Schema({
     user: { type: SchemaTypes.ObjectId, ref: "User" },
-    comment: { type: String, maxlength: 200 }
+    comment: { type: String, maxlength: 200 },
+    concert: {type: SchemaTypes.ObjectId, ref: "Concert" }
   });
 
   module.exports = model('Comment', commentSchema);
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
-
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Comment = require('../models/Comment.model');
 const Concert = require('../models/Concert.model');
 
 //  POST /api/comments  -  Creates a new comment
-router.post('/comments', (req, res, next) => {
-  const { user, comment } = req.body;
+router.post('/comments', isAuthenticated, (req, res, next) => {
+  console.log(req.payload._id)
+  
+  const userId = req.payload._id
+  const { user, comment, concert } = req.body;
 
-  Comment.create({ user, comment, concert: concertId })
+  Comment.create({ user, comment, concert })
     .then(newComment => {
-      return Concert.findByIdAndUpdate(concertId, { $push: { comments: newComment._id } } );
+      return Concert.findByIdAndUpdate(concert, { $push: { comments: newComment._id } } );
     })
     .then(response => res.json(response))
     .catch(err => res.json(err));
@@ -22,4 +25,96 @@ router.get('/comments', (req, res, next) => {
     .then(allConcerts => res.json(allConcerts))
     .catch(err => res.json(err));
 });
+
+
+router.get('/:id/comment', (req, res, next) => {
+
+  const {id} = req.params
+
+Concert.findById(id)
+
+  .then(foundConcert => res.render('concerts/comment', foundConcert))
+  .catch(err => console.log(err))
+
+});
+
+router.post('/:concertId/comment', isAuthenticated, (req, res, next) => {
+  console.log(req.payload._id)
+
+  const userId = req.payload._id
+  const {comment} = req.body
+  console.log(comment)
+  
+  const {concertId} = req.params
+  console.log("USER ID", userId)
+
+  if (!comment) {
+      res.render('/:id/comment', { errorMessage: 'Please write a comment before sending the form.' });
+      return;
+    }
+
+    Comment.create({user: userId, comment, concert: concertId})
+      .then((newComment) => {
+        console.log(concertId)
+          Concert.findById(concertId)
+          
+              .then((commentedConcert) => {
+                  commentedConcert.comments.push(newComment._id) 
+                  commentedConcert.save()
+              })
+              .catch(err => console.log(err))
+      })
+      .then(() => res.redirect('/concerts'))
+      .catch(err => console.log(err))
+
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const router = require("express").Router();
+
+// const { isAuthenticated } = require("../middleware/jwt.middleware");
+// const Comment = require('../models/Comment.model');
+// const Concert = require('../models/Concert.model');
+
+// //  POST /api/comments  -  Creates a new comment
+// router.post('/comments', isAuthenticated, (req, res, next) => {
+//   console.log(req.payload._id)
+  
+//   const userId = req.payload._id
+//   const { user, comment, concert } = req.body;
+
+//   Comment.create({ user, comment, concert })
+//     .then(newComment => {
+//       return Concert.findByIdAndUpdate(concert, { $push: { comments: newComment._id } } );
+//     })
+//     .then(response => res.json(response))
+//     .catch(err => res.json(err));
+// });
+
+// // GET /api/comment -  Retrieves all of the concerts
+// router.get('/comments', (req, res, next) => {
+//   Concert.find()
+//     .populate('comments')
+//     .then(allConcerts => res.json(allConcerts))
+//     .catch(err => res.json(err));
+// });
+// module.exports = router;
