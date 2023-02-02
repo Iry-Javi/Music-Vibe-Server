d5ed213043c9913eece0cd59a2dcf3e663a1e085
Iryna1791 <irynawebdev@gmail.com>
2023-02-02 15:17:00 +0100

    update edit, delite, create and profile

--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
 const Concert =  require('../models/Concert.model');
 const User = require('../models/User.model');
-const Comment = require('../models/Comment.model');
-const { Console } = require('console');
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+const jwt = require("jsonwebtoken");
 
 
 router.post('/concerts', isAuthenticated, (req, res) => {
   const { title, image, description,  country, city, street, houseNumber, postalCode, comment } = req.body;
   const userId = req.payload._id
-console.log(req.body)
+
   Concert.create({ title, image, description, country, city, street, houseNumber, postalCode, comment: [] })
   .then(newConcert => {
-      console.log(newConcert)
       return User.findByIdAndUpdate(userId, {
         $push: { concert: newConcert._id },
       },{new: true})
+      .populate("concert")
     })
-      .then((updatedUser) => res.json(updatedUser))
+      .then((updatedUser) => {
+        const {_id, username, image, concert} = updatedUser;
+        const payload = {_id, username, image, concert };
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+        res.json( { updatedUser: payload, authToken })})
       .catch(err => res.json(err));
 });
 
 // GET /api/concerts -  Retrieves all of the concerts
 router.get('/concerts', isAuthenticated, (req, res, next) => {
-  console.log(req.payload._id)
   Concert.find()
     .populate('comments')
     .then(allConcerts => res.json(allConcerts))
@@ -36,7 +40,6 @@ router.get('/concerts', isAuthenticated, (req, res, next) => {
 //  GET /api/concerts/:concertId -  Retrieves a specific project by id
 router.get('/concerts/:concertId', (req, res, next) => {
   const { concertId } = req.params;
- 
   if (!mongoose.Types.ObjectId.isValid(concertId)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
@@ -58,78 +61,56 @@ router.get('/concerts/:concertId', (req, res, next) => {
 });
 
 // PUT  /api/concerts/:concertId  -  Updates a specific project by id
-router.put('/concerts/:concertId', (req, res, next) => {
+router.put('/concerts/:concertId', isAuthenticated, (req, res, next) => {
   const { concertId } = req.params;
- 
+  const userId = req.payload._id;
   if (!mongoose.Types.ObjectId.isValid(concertId)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
   }
  
   Concert.findByIdAndUpdate(concertId, req.body, { new: true })
-    .then((updatedConcert) => res.json(updatedConcert))
+    .then(updatedConcert => {     // update the user state on the front end
+      return User.findById(userId).populate("concert")
+      })
+      .then((updatedUser) => {
+        const {_id, username, image, concert} = updatedUser;
+        const payload = {_id, username, image, concert };
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+        res.json( { updatedUser: payload, authToken })
+      })
     .catch(error => res.json(error));
 });
 
 // DELETE  /api/projects/:projectId  -  Deletes a specific project by id
-router.delete('/concerts/:concertId', (req, res, next) => {
+router.delete('/concerts/:concertId', isAuthenticated, (req, res, next) => {
   const { concertId } = req.params;
-  
+  const userId = req.payload._id;
   if (!mongoose.Types.ObjectId.isValid(concertId)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
   }
  
   Concert.findByIdAndRemove(concertId)
-    .then(() => res.json({ message: `Concert with ${concertId} is removed successfully.` }))
+  .then(deletedConcert => {
+    return User.findByIdAndUpdate(userId, {
+      $pull: { concert: deletedConcert._id },
+    },{new: true})
+    .populate("concert")
+  })
+  .then((updatedUser) => {
+    const {_id, username, image, concert} = updatedUser;
+    const payload = {_id, username, image, concert };
+    const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "6h",
+    });
+    res.json( { updatedUser: payload, authToken })
+  })  
     .catch(error => res.json(error));
 });
 
 module.exports = router;
-
-//COMMENT
-// router.get('/:id/comment', (req, res, next) => {
-
-//   const {id} = req.params
-
-// Concert.findById(id)
-
-//   .then(foundConcert => res.render('concerts/comment', foundConcert))
-//   .catch(err => console.log(err))
-
-// });
-
-// router.post('/:concertId/comment', isAuthenticated, (req, res, next) => {
-//   console.log(req.payload._id)
-
-//   const userId = req.payload._id
-//   const {comment} = req.body
-//   console.log(comment)
-  
-//   const {concertId} = req.params
-//   console.log("USER ID", userId)
-
-//   if (!comment) {
-//       res.render('/:id/comment', { errorMessage: 'Please write a comment before sending the form.' });
-//       return;
-//     }
-
-//     Comment.create({user: userId, comment, concert: concertId})
-//       .then((newComment) => {
-//         console.log(concertId)
-//           Concert.findById(concertId)
-          
-//               .then((commentedConcert) => {
-//                   commentedConcert.comments.push(newComment._id) 
-//                   commentedConcert.save()
-//               })
-//               .catch(err => console.log(err))
-//       })
-//       .then(() => res.redirect('/concerts'))
-//       .catch(err => console.log(err))
-
-// });
-
-
-
-
